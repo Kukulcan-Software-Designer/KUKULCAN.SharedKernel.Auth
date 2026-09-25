579a979e0ac3601a607376677269c66e6191e535
Juan Manuel Pardo García <jpardo.kukulcan@gmail.com>
2026-09-25 20:49:28 +0200

    test: fix Google credential validator test namespace

--- Tests/KUKULCAN.SharedKernel.Auth.UnitTests/ValidatorTests/GoogleCredentialValidatorTests.cs
+++ Tests/KUKULCAN.SharedKernel.Auth.UnitTests/ValidatorTests/GoogleCredentialValidatorTests.cs
@@ -3,6 +3,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Text.Json;
 using NUnit.Framework;
+using KUKULCAN.SharedKernel.Auth.Authentication.Federated;
 
 namespace KUKULCAN.SharedKernel.Auth.UnitTests.ValidatorTests;
 
